26dfecb9c987f17314fe2691211d7a0253285c62
Abiria <abiria.dev@gmail.com>
2024-10-05 17:43:16 +0900

    feat: remove all unnecessary types

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,4 @@
-use std::{
-    error::Error,
-    fmt::{Debug, Display, Formatter},
-    sync::Arc,
-};
+use std::sync::Arc;
 
 use time::{Date, Duration, PrimitiveDateTime};
 
@@ -20,62 +16,8 @@ pub struct UserStatistics {
     pub calendar: Vec<(Date, Duration)>,
 }
 
-pub struct VcEvent {
-    id: u64,
-    at: u64,
-    event_kind: VcEventKind,
-}
-
-#[repr(u64)]
-pub enum VcEventKind {
-    Join = 0,
-    Leave,
-    Detected,
-}
-
-pub struct TerminationEvent {
-    exit_code: u8,
-}
-
 pub struct Db {
     config: Arc<Config>,
-}
-
-pub enum VcDBError {
-    InvalidId,
-    InvalidAt,
-    InvalidKind,
-}
-
-impl VcEvent {
-    pub fn from_db(value1: u64, value2: u64, value3: u8) -> Result<Self, VcDBError> {
-        if value1 != 0 {
-            return Err(VcDBError::InvalidId);
-        }
-
-        if value2 == 0 {
-            return Err(VcDBError::InvalidAt);
-        }
-
-        let event_kind = match value3 {
-            0 => VcEventKind::Join,
-            1 => VcEventKind::Leave,
-            2 => VcEventKind::Detected,
-            _ => return Err(VcDBError::InvalidKind),
-        };
-
-        Ok(Self {
-            id: value1,
-            at: value2,
-            event_kind,
-        })
-    }
-}
-
-impl TerminationEvent {
-    pub fn from_db(value3: u8) -> Self {
-        Self { exit_code: value3 }
-    }
 }
 
 impl Db {
@@ -84,19 +26,14 @@ impl Db {
     }
 
     // when user joins
-    pub async fn joins(user: u64, when: PrimitiveDateTime) -> anyhow::Result<()> {
+    pub async fn joins(user: u64) -> anyhow::Result<()> {
         println!("user {} joined!", user);
         Ok(())
     }
 
     // when user leaves
-    pub async fn leaves(user: u64, when: PrimitiveDateTime) -> anyhow::Result<()> {
+    pub async fn leaves(user: u64) -> anyhow::Result<()> {
         println!("user {} left!", user);
-        Ok(())
-    }
-
-    pub async fn detected(user: u64) -> anyhow::Result<()> {
-        println!("user {} is already in vc!", user);
         Ok(())
     }
 
@@ -109,27 +46,4 @@ impl Db {
     pub async fn user_statistics() -> anyhow::Result<UserStatistics> {
         todo!()
     }
-
-    pub async fn on_crash(exit_code: u8) -> anyhow::Result<()> {
-        todo!()
-    }
 }
-
-impl Debug for VcDBError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let message = match self {
-            VcDBError::InvalidId => "Invalid User Id.",
-            VcDBError::InvalidAt => "Invalid Timestamp.",
-            VcDBError::InvalidKind => "Invalid Event Kind.",
-        };
-        f.write_str(message)
-    }
-}
-
-impl Display for VcDBError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str("Invalid Data found.")
-    }
-}
-
-impl Error for VcDBError {}
