a930261eb5a57c6dfae9a0f3d18878ef2e63d38d
tmvkrpxl0 <tmvkrpxl0@proton.me>
2024-10-06 07:07:57 +0900

    oops2

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+use std::fmt::Debug;
+
+use log::error;
+
+pub use bot::*;
+pub use config::*;
+
 mod bot;
 mod config;
 mod db;
 mod utils;
-
-pub use bot::*;
-pub use config::*;
-use std::fmt::Debug;
 
 pub trait LogUtil<T> {
     fn report_on_error(self) -> Option<T>;
