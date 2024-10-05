f517c6d0b19ace6cb91973444c284f7528298e68
Abiria <abiria.dev@gmail.com>
2024-10-06 04:49:12 +0900

    refactor: `string` to `text`

--- migrations/20241005132758_init.sql
+++ migrations/20241005132758_init.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 create table if not exists vc_activities (
 	`id` integer primary key,
-	`user` string not null,
+	`user` text not null,
 	`joined` datetime not null default current_timestamp,
 	`left` datetime
 );
