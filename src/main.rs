dc2efeab173057a450e6ceaccf49067860e9653d
Abiria <abiria.dev@gmail.com>
2024-10-06 05:59:51 +0900

    feat: validate token before run

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use std::env::var;
 use dotenvy::dotenv;
 
 use mogakko_bot::{Bot, Config};
+use serenity::all::validate_token;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv()?;
 
+    let token = var("DISCORD_TOKEN").expect("Environment Variable DISCORD_TOKEN not found!");
+    validate_token(&token)?;
+
     let config = Config {
-        token: var("DISCORD_TOKEN").expect("Environment Variable DISCORD_TOKEN not found!"),
+        token,
         vc_id: var("CHANNEL_ID")
             .expect("Environment Variable CHANNEL_ID not found!")
             .parse()
