a3d2c8fe68222620b7b79cd65d7af9d8b44f0171
Abiria <abiria.dev@gmail.com>
2024-10-05 22:11:34 +0900

    refactor: main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::env::var;
 
 use dotenvy::dotenv;
 use mogakko_bot::{Bot, Config};
@@ -8,12 +8,12 @@ async fn main() -> anyhow::Result<()> {
     dotenv()?;
 
     let config = Config {
-        token: env::var("DISCORD_TOKEN").expect("Environment Variable DISCORD_TOKEN not found!"),
-        vc_id: env::var("CHANNEL_ID")
+        token: var("DISCORD_TOKEN").expect("Environment Variable DISCORD_TOKEN not found!"),
+        vc_id: var("CHANNEL_ID")
             .expect("Environment Variable CHANNEL_ID not found!")
             .parse()
             .unwrap(),
-        database_url: env::var("DATABASE_URL").unwrap(),
+        database_url: var("DATABASE_URL").unwrap(),
     };
 
     let mut bot = Bot::new(config).await?;
