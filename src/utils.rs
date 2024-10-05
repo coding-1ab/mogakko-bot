8dfe6d08495c80556a7901278151459e8929c465
Abiria <abiria.dev@gmail.com>
2024-10-05 23:53:04 +0900

    fix: macro feature

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,10 @@
-use time::{OffsetDateTime, UtcOffset};
+use time::{macros::offset, OffsetDateTime};
 
 pub fn now_kst() -> OffsetDateTime {
     OffsetDateTime::now_utc().to_offset(offset!(+9))
 }
 
-pub fn is_in_target_range(when: OffsetDateTime) -> bool {
+pub fn is_valid_time(when: OffsetDateTime) -> bool {
     let h = when.hour();
 
     h >= 18 && h < 22
